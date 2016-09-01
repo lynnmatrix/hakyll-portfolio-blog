9a235a1d2e0edc004f5632f805cf7ac430bf754a
Rob Whitaker <robjameswhitaker@gmail.com>
2016-08-31 21:12:19 -0400

    Prettified all the generated URLs and fixed the links to point to the new URLs.

--- site.hs
+++ site.hs
@@ -2,7 +2,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
-
+import           System.FilePath (takeDirectory,takeBaseName,(</>))
+import           Data.List (isSuffixOf)
+import           Text.Regex (mkRegex,splitRegex)
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -16,20 +18,25 @@ main = hakyll $ do
         compile compressCssCompiler
 
     match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
+        route stripExtensionRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+            >>= tidyUrls
 
     match "posts/*" $ do
-        route $ setExtension "html"
+        route tidyRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+            >>= tidyUrls
+
+    --match "projects/*" $ do
+    --    route $ customRoute $ (\fName -> "projects/" ++ fName ++ "/index.html") . takeBaseName . toFilePath
+    --    compile $ pandocCompiler
+    --        >>= loadAndApplyTemplate
 
     create ["archive.html"] $ do
-        route idRoute
+        route stripExtensionRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
@@ -40,7 +47,7 @@ main = hakyll $ do
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+                >>= tidyUrls
 
 
     match "index.html" $ do
@@ -55,13 +62,58 @@ main = hakyll $ do
             getResourceBody
                 >>= applyAsTemplate indexCtx
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+                >>= tidyUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
 
 --------------------------------------------------------------------------------
+
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+--------------------------------------------------------------------------------
+
+stripExtensionRoute :: Routes
+stripExtensionRoute = customRoute createIndexRoute
+  where
+    createIndexRoute ident = takeDirectory p ++ "/" ++ takeBaseName p ++ "/index.html"
+                            where p = toFilePath ident
+
+cleanIndex :: String -> String
+cleanIndex url
+    | idx `isSuffixOf` url = take (length url - length idx) url
+    | otherwise            = url
+  where idx = "index.html"
+
+cleanIndexUrls :: Item String -> Compiler (Item String)
+cleanIndexUrls = return . fmap (withUrls cleanIndex)
+
+cleanIndexHtmls :: Item String -> Compiler (Item String)
+cleanIndexHtmls = return . fmap (replaceAll "/index.html" $ const "/")
+
+stripHtmlExtensions :: Item String -> Compiler (Item String)
+stripHtmlExtensions = return . fmap (withUrls $ replaceAll ".html" (const "/"))
+
+stripDateRoute :: Routes
+stripDateRoute = customRoute $ stripDate . toFilePath
+
+stripDate :: String -> String
+stripDate = concat . splitRegex (mkRegex "([0-9]{4})\\-([0-9]{2})\\-([0-9]{2})\\-")
+
+stripDateUrls :: Item String -> Compiler (Item String)
+stripDateUrls = return . fmap (withUrls stripDate)
+
+tidyRoute :: Routes
+tidyRoute =
+    stripExtensionRoute `composeRoutes`
+    stripDateRoute
+
+tidyUrls :: Item String -> Compiler (Item String)
+tidyUrls item = return item
+    >>= relativizeUrls
+    >>= cleanIndexUrls
+    >>= stripHtmlExtensions
+    >>= stripDateUrls
